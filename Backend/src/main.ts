13d89ebd226af59afd1822950c3a172e1d57b7b4
Sanjeev Kumar <sanjeev.kumar1@infinitylearn.com>
2026-04-17 15:18:47 +0530

    Backedn changes to allow api methods

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
 
