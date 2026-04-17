2a9319f36fa1f0dbcc22cb58751158f91f462ba2
Sanjeev Kumar <sanjeev.kumar1@infinitylearn.com>
2026-04-17 14:07:52 +0530

    Changes to pick backend URL from env

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,19 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  console.log("modevalue", mode);
+  // Load env file based on `mode` (development, production, etc.)
+  // process.cwd() tells Vite to look for .env in the project root
+  const env = loadEnv("", process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: { '@': path.resolve(__dirname, './src') },
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          // Use the loaded env variable here
+          target: env.VITE_BACKEND_URL || 'http://localhost:3001',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
